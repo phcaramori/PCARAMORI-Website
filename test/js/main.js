a6afc732bcfc80b5f80b3144502a20ae76d8afe2
phcaramori <phcaramori@gmail.com>
2020-10-21 16:29:27 -0300

    added flags and multi-language support

--- test/js/main.js
+++ test/js/main.js
@@ -2,30 +2,42 @@ function showNav(){
     document.getElementById('nav-window').style.display = 'flex';
     document.getElementById('nav-button').style.display = 'none';
     document.getElementById('close-nav').style.display = 'inline-block';
+    document.getElementsByClassName('flags')[0].style.display = '';
+
     //animating menu 
-    document.getElementById('nav-window').classList.remove("menu-back")
+    document.getElementById('nav-window').classList.remove("menu-back");
     document.getElementById('nav-window').classList.add("from-left");
     let menuLine = document.getElementsByClassName('menu-line');
     for(let i = 0; i<menuLine.length; i++){
-        menuLine[i].classList.add('hr-anim');
+        menuLine[i].classList.add('hr-anim'); //add "hr-anim" to all lines in the nav menu. (the green lines that go from left to right as the menu comes in)
     }
-    //setTimeout(function(){document.getElementById('nav-window').style.position = ''},1000)
 }
+
 function closeNav(){
-    document.getElementById('nav-window').classList.add("menu-back")
+    //animating menu
+    document.getElementById('nav-window').classList.add("menu-back");
+    
+    //hiding menu
     setTimeout(function(){
         document.getElementById('nav-button').style.display = 'inline-block';
         document.getElementById('close-nav').style.display = 'none';
         document.getElementsByClassName('menu-line');
-    //animating menu
+        document.getElementsByClassName('flags')[0].style.display = 'none'
+        document.getElementById('nav-window').style.display = 'none';
+
+    //removing animations from menu items
         document.getElementById('nav-window').classList.remove("from-left");
         let menuLine = document.getElementsByClassName('menu-line');
-        for(let i = 0; i<menuLine.length; i++){
-            menuLine[i].classList.remove('hr-anim');
-        }
-    } ,300) 
-    setTimeout(function(){
         document.getElementById('nav-window').style.display = 'none';
-    },900)
-
+        for(let i = 0; i<menuLine.length; i++){
+            menuLine[i].classList.remove('hr-anim'); //remove "hr-anim" from all lines in the nav menu. (the green lines that go from left to right as the menu comes in)
+        }
+    } ,300) //wait 300ms before hiding menu so animation can complete
+}
+//
+function lockScroll(){
+    document.getElementsByTagName('body')[0].classList.add('noScroll')
+}
+function allowScroll(){
+    document.getElementsByTagName('body')[0].classList.remove('noScroll')
 }

9f912d773fff2126e64bdcb33cc4106083a3a7fb
Pedro Caramori <phcaramori@gmail.com>
2021-03-31 01:28:18 -0300

    fixed numToCard function to return full card names

--- test/en/projects/cardGameStuff/index.js
+++ test/en/projects/cardGameStuff/index.js
@@ -21,10 +21,9 @@ let Game = {
 
     //display functions
     display : {
-        displayCard : function(div,cardNum){ // put . in front of div name when calling function, Ex: .first
-            //displays a specified card
+        displayCard : function(div,card){ // put . in front of div name when calling function, Ex: .first
+            //displays a specified card. Must call with the card's object
             let element = document.createElement('img');
-            let card = Game.deck[cardNum]
             element.setAttribute('src',`./res/${card.number}_of_${card.suit}.png`); //add src to image
             document.querySelector(div).appendChild(element) //create img element in given div
         },
@@ -69,10 +68,10 @@ function init(numberOfDecks,joker){ //makes all cards | int, bool
     }
     for(let k = 0; k<numberOfDecks; k++){ //repeat for how many decks should be put in the shuffle*/
         for(let i = 1; i<14; i++){ //add 4 cards to deck, one of each suit
-            Game.deck.push(new Game.Card("hearts",Game.background.numToCard(i)));
-            Game.deck.push(new Game.Card("spades",Game.background.numToCard(i)));
-            Game.deck.push(new Game.Card("clubs",Game.background.numToCard(i)));
-            Game.deck.push(new Game.Card("diamonds",Game.background.numToCard(i)));
+            Game.deck.push(new Game.Card("hearts",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("spades",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("clubs",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("diamonds",Game.numToCard(i)));
         }
         if(joker){ //add two jokers per deck
             Game.deck.push(new Game.Card("joker","joker"));

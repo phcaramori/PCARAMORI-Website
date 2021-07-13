02a741a2bffd0db93ecb59589a7f17ca2b5067ed
Pedro Caramori <phcaramori@gmail.com>
2021-07-13 18:21:14 -0300

    restructured files

---
+++ test/projects/cardGameStuff/index.js
@@ -0,0 +1,104 @@
+//idea: have one div with all cards, which can be fully deleted and changed without changing the webpage
+let Game = {
+    deck: [], //the deck with all the playing cards for the game
+    shuffledDeck: [], //the shuffled deck of cards for the game
+
+    //constructors
+    Card : function(suit, number){ //card constructor
+        this.suit = suit;
+        this.number = number;
+    },
+
+    shuffle : function(){ //shuffle the deck of cards
+        let final = []; //output array
+        let temp = [...Game.deck];
+        for (let i = temp.length; i > 0; i--) { //loop from end of array to beginning
+            let random = Math.floor(Math.random()*temp.length) //make rand num up to array length
+            final.push(temp[random]) //get a random index from temp, push it to final
+            temp.splice(random,1) //remove that index
+        }
+        Game.shuffledDeck = final;
+    },
+
+    //display functions
+    display : {
+        displayCard : function(element,card){ 
+            //displays a specified card. Must call with the card's object
+            let img = document.createElement('img');
+            img.setAttribute('src',`./res/${card.number}_of_${card.suit}.png`); //add src to image
+            img.setAttribute('class','card')
+            element.appendChild(img) //create img element in given div
+        },
+        displayAll : function(element, deck){ 
+            //displays all cards in the deck, in order.
+            for (let i = 0; i < deck.length; i++) {
+                this.displayCard(element, deck[i])
+            }
+        },
+        displayFrom : function(element, deck, min, max){
+            //displays certain cards in a deck.
+            //if only 1 min/max value is specified, it gets the first __ cards from a deck. Doesn't remove from the deck.
+            if(!max){
+                max = min
+                min = 0;;
+            }
+            for (let i = min; i < max; i++) {
+                this.displayCard(element, deck[i])
+            }
+        },
+        clear : function(element){ //remove all children from element
+            while (element.childElementCount) {
+                element.removeChild(element.childNodes[0])
+            } //while there still are children, remove them
+        }
+    },
+    numToCard : function(num){ //turns inputted number into corresponding playing card number (Ex: 11 = J)
+        switch (num) {
+            case 1:
+                return "ace"
+                break;
+    
+            case 11:
+                return "jack"
+                break;
+        
+            case 12:
+                return "queen"
+                break;
+        
+            case 13:
+                return "king"
+                break;
+        
+            default:
+                return num
+                break;
+        }
+    },
+
+}
+
+
+
+function init(numberOfDecks,joker){ //makes all cards | int, bool
+    if(!numberOfDecks){ //default numberOfDecks to 1 if not specified
+        numberOfDecks = 1;
+    }
+    for(let k = 0; k<numberOfDecks; k++){ //repeat for how many decks should be put in the shuffle*/
+        for(let i = 1; i<14; i++){ //add 4 cards to deck, one of each suit
+            Game.deck.push(new Game.Card("hearts",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("spades",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("clubs",Game.numToCard(i)));
+            Game.deck.push(new Game.Card("diamonds",Game.numToCard(i)));
+        }
+        if(joker){ //add two jokers per deck
+            Game.deck.push(new Game.Card("red","joker"));
+            Game.deck.push(new Game.Card("black","joker"));
+        }
+    }
+    console.log(Game.deck)
+}
+
+function $(selector){
+    return document.querySelector(selector)
+}

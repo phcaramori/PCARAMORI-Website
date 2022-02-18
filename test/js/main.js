c288eca102cc63918fd9d417e6d37633f83c6b7e
Pedro Caramori <Pedrocaramori@MacBook-de-MacBook-2.local>
2022-02-18 12:29:47 -0300

    fixed z-index conflicts

--- test/js/main.js
+++ test/js/main.js
@@ -6,7 +6,7 @@
 
 */
 
-function showNav(){
+function showNav() {
     document.getElementById('nav-window').style.display = 'flex';
     document.getElementById('nav-button').style.display = 'none';
     document.getElementById('close-nav').style.display = 'inline-block';
@@ -14,48 +14,49 @@ function showNav(){
 
     //animating menu 
     document.getElementById('nav-window').classList.remove("menu-back");
-    document.getElementById('nav-window').classList.add("from-left");
+    document.getElementById('nav-window').classList.add("menu-from-left");
     let menuLine = document.getElementsByClassName('menu-line');
-    for(let i = 0; i<menuLine.length; i++){
+    for (let i = 0; i < menuLine.length; i++) {
         menuLine[i].classList.add('hr-anim'); //add "hr-anim" to all lines in the nav menu. (the green lines that go from left to right as the menu comes in)
     }
 }
 
-function closeNav(){
+function closeNav() {
     //animating menu
     document.getElementById('nav-window').classList.add("menu-back");
-    
-    //hiding menu
-    setTimeout(function(){
-        document.getElementById('nav-button').style.display = 'inline-block';
-        document.getElementById('close-nav').style.display = 'none';
-        document.getElementsByClassName('menu-line');
-        document.getElementsByClassName('flags')[0].style.display = 'none'
-        document.getElementById('nav-window').style.display = 'none';
 
-    //removing animations from menu items
-        document.getElementById('nav-window').classList.remove("from-left");
-        let menuLine = document.getElementsByClassName('menu-line');
-        document.getElementById('nav-window').style.display = 'none';
-        for(let i = 0; i<menuLine.length; i++){
-            menuLine[i].classList.remove('hr-anim'); //remove "hr-anim" from all lines in the nav menu. (the green lines that go from left to right as the menu comes in)
-        }
-    } ,300) //wait 300ms before hiding menu so animation can complete
+    //hiding menu
+    setTimeout(function() {
+            document.getElementById('nav-button').style.display = 'inline-block';
+            document.getElementById('close-nav').style.display = 'none';
+            document.getElementsByClassName('menu-line');
+            document.getElementsByClassName('flags')[0].style.display = 'none'
+            document.getElementById('nav-window').style.display = 'none';
+
+            //removing animations from menu items
+            document.getElementById('nav-window').classList.remove("menu-from-left");
+            let menuLine = document.getElementsByClassName('menu-line');
+            document.getElementById('nav-window').style.display = 'none';
+            for (let i = 0; i < menuLine.length; i++) {
+                menuLine[i].classList.remove('hr-anim'); //remove "hr-anim" from all lines in the nav menu. (the green lines that go from left to right as the menu comes in)
+            }
+        }, 300) //wait 300ms before hiding menu so animation can complete
 }
 //
-function lockScroll(){
+function lockScroll() {
     document.getElementsByTagName('body')[0].classList.add('noscroll')
 }
-function allowScroll(){
+
+function allowScroll() {
     document.getElementsByTagName('body')[0].classList.remove('noscroll')
 }
 
 // pop-ups
 
-function popUp(text,head){
+function popUp(text, head) {
     //lock scrolling
     lockScroll();
-    
+
     //create div and give it the pop-up class
     let newPopUp = document.createElement("div");
     document.body.appendChild(newPopUp);
@@ -68,25 +69,25 @@ function popUp(text,head){
     newPopUp.appendChild(hr);
     let p = document.createElement("p");
     newPopUp.appendChild(p);
-    
+
     //give h1 and p elements their text
     p.innerHTML = text;
-    if(head){
+    if (head) {
         h1.innerHTML = head;
-    }else{
+    } else {
         h1.innerHTML = "Warning!"
     }
 
     // pop-up removal
-    document.addEventListener('click', function(event){
+    document.addEventListener('click', function(event) {
         let isClickInside = newPopUp.contains(event.target);
-        if(!isClickInside){ //if the click isn't inside the pop-up, hide it.
+        if (!isClickInside) { //if the click isn't inside the pop-up, hide it.
             hidePopUp();
         }
     })
 }
 
-function hidePopUp(){
+function hidePopUp() {
     document.getElementsByClassName("pop-up")[0].remove();
     allowScroll();
 }

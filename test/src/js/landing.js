d9516e6a17712f0b9a00558dc6cace176ac4900e
Pedro Caramori <phcaramori@gmail.com>
2022-09-26 00:52:56 -0300

    removed rotation effect on boxes :(

--- test/src/js/landing.js
+++ test/src/js/landing.js
@@ -35,24 +35,3 @@ function intro() {
 }
 intro()
 
-
-//move intro box around mouse
-
-// const anchor = document.getElementsByClassName('hero-title')[0]
-//  //get coordinates for Y center of logo
-// document.addEventListener('mousemove',(e) => {
-
-//     const rect = anchor.getBoundingClientRect()
-//     const anchorX = rect.left + rect.width / 2; //get coordinates for X center of logo
-//     const anchorY = rect.top + rect.height / 2;
-
-//     //get  position of  mouse relative to the center of the logo
-//     const x = e.clientX - anchorX; 
-//     const y = e.clientY - anchorY;
-
-//     const rad = Math.atan2(x,y);
-//     let deg = rad * 180 / Math.PI;
-//     deg += 90
-//     deg *= -1
-//     anchor.style.transform = `rotate(${deg}deg)`;
-// })
